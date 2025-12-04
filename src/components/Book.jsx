739f6a73f0af637940f7ceb0496b6a9fca5a4ea6
Feileen Li <fli1234@terpmail.umd.edu>
2025-12-03 21:18:39 -0500

    trying again

---
+++ src/components/Book.jsx
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from 'react'
+import HTMLFlipBook from 'react-pageflip';
+
+function Book() {
+    const journalEntries = [
+
+    ]
+    
+    return (
+        <HTMLFlipBook width={300} height={500}>
+        <div className="demoPage">Page 1</div>
+        <div className="demoPage">Page 2</div>
+        <div className="demoPage">Page 3</div>
+        <div className="demoPage">Page 4</div>
+        </HTMLFlipBook>
+    );
+
+
+
+
+}
+
+export default Book;
